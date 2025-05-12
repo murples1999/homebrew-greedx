c24d5a54371f0de48f114c5ee11735072d7e88ee
murples1999 <ffej1999@gmail.com>
2025-05-12 03:07:55 -0700

    Fix SHA256 for Homebrew install

--- Formula/greedx.rb
+++ Formula/greedx.rb
@@ -1,7 +1,7 @@
 class Greedx < Formula
   desc "Terminal-based puzzle game like classic Greed"
   homepage "https://github.com/murples1999/greedx"
-  url "https://github.com/murples1999/greedx/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/murples1999/greedx/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "61344880d118733cb41de58cc24288373cb6e218de1b52b9a6380ebfd00e5a53"
   license "MIT"
 
