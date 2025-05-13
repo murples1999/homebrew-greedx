c67598238086fd37a7c6a32489a733f2043619eb
murples1999 <ffej1999@gmail.com>
2025-05-12 17:58:52 -0700

    Update formula to v1.0.2

--- Formula/greedx.rb
+++ Formula/greedx.rb
@@ -1,8 +1,8 @@
 class Greedx < Formula
   desc "Terminal-based puzzle game like classic Greed"
   homepage "https://github.com/murples1999/greedx"
-  url "https://github.com/murples1999/greedx/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "61344880d118733cb41de58cc24288373cb6e218de1b52b9a6380ebfd00e5a53"
+  url "https://github.com/murples1999/greedx/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "d3ba85bb49942114ec6b3b5de6ce3c18532aafb4e1c0a5dbf2dd7a374cbdd85b"
   license "MIT"
 
   depends_on "ncurses"
